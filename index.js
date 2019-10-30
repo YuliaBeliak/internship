b93a2c9df03f31845dfac8634b6c862250dfe3fe
YuliaBeliak <belyakzhe@gmail.com>
2019-10-30 20:29:34 +0300

    add function for substring replacement

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ function reverseString(str) {
     return str.split('').reverse().join('');
 }
 
+function replaceSubStr(initialStr, subStr, newSubStr) {
+    let reg = new RegExp(subStr, 'gi');
+    return initialStr.replace(reg, newSubStr);
+}
+
