f578ac7c56faeab7369f6ec38bf99bd6ab6d881d
YuliaBeliak <belyakzhe@gmail.com>
2019-11-05 21:08:47 +0300

    add task3

---
+++ task3/app.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+
+
+app.use(bodyParser.text());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', (req, res) => {
+    res.send('Hello!');
+});
+
+app.get('/:file', (req, res) => {
+    fs.readFile(path.join(__dirname, `./files${req.url}.txt`), 'utf-8', (err, data) => {
+        if (err) {
+            res.end(`File '${req.url}.txt' does not exist`);
+        }
+        res.send(data);
+    })
+});
+
+app.post('/:file', (req, res) => {
+    fs.access(path.join(__dirname, `./files${req.url}.txt`), fs.constants.F_OK, (err) => {
+        if (err) {
+            fs.writeFile(path.join(__dirname, `./files${req.url}.txt`), req.body, (err) => {
+                if (err) {
+                    console.log(err);
+                }
+                res.send('The file has been saved');
+            });
+        } else {
+            res.send('The file already exist');
+        }
+    });
+});
+
+app.put('/:file', (req, res) => {
+    fs.access(path.join(__dirname, `./files${req.url}.txt`), fs.constants.F_OK, (err) => {
+        if (err) {
+            res.send('File does not exist')
+        } else {
+            fs.appendFile(path.join(__dirname, `./files${req.url}.txt`), `\n${req.body}`, (err) => {
+                if (err) {
+                    throw err;
+                } else {
+                    res.send(`file has been updated`);
+                }
+            });
+        }
+    });
+});
+
+app.delete('/:file', (req, res) => {
+    fs.access(path.join(__dirname, `./files${req.url}.txt`), fs.constants.F_OK, (err) => {
+        if (err) {
+            res.send('File does not exist');
+        } else {
+            fs.unlink(path.join(__dirname, `./files${req.url}.txt`), (err) => {
+                if (err) throw err;
+                res.send(`file has been deleted`);
+            });
+        }
+    });
+});
+
+app.listen('3000', () => console.log('Server is running on port 3000'));
