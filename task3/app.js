360e30b902952482994dde007b196c107fd50a91
YuliaBeliak <belyakzhe@gmail.com>
2019-11-06 10:31:21 +0300

    refactoring: extract existence check into separate fn

--- task3/app.js
+++ task3/app.js
@@ -23,45 +23,53 @@ app.route('/:file')
         })
     })
     .post((req, res) => {
-        fs.access(path.join(__dirname, `./files${req.url}.txt`), fs.constants.F_OK, (err) => {
-            if (err) {
-                fs.writeFile(path.join(__dirname, `./files${req.url}.txt`), req.body, (err) => {
-                    if (err) {
-                        console.log(err);
-                    }
-                    res.send('The file has been saved');
-                });
-            } else {
-                res.send('The file already exist');
-            }
-        });
+        if (checkExistence(req.url)) {
+            res.send('The file already exist');
+        } else {
+            fs.writeFile(path.join(__dirname, `./files${req.url}.txt`), req.body, (err) => {
+                if (err) {
+                    console.log(err);
+                }
+                res.send('The file has been saved');
+            });
+        }
     })
     .put((req, res) => {
-        fs.access(path.join(__dirname, `./files${req.url}.txt`), fs.constants.F_OK, (err) => {
-            if (err) {
-                res.send('File does not exist')
-            } else {
-                fs.appendFile(path.join(__dirname, `./files${req.url}.txt`), `\n${req.body}`, (err) => {
-                    if (err) {
-                        throw err;
-                    } else {
-                        res.send(`file has been updated`);
-                    }
-                });
-            }
-        });
+        if (checkExistence(req.url)) {
+            fs.appendFile(path.join(__dirname, `./files${req.url}.txt`), `\n${req.body}`, (err) => {
+                if (err) {
+                    throw err;
+                } else {
+                    res.send(`file has been updated`);
+                }
+            });
+        } else {
+            res.send('File does not exist')
+        }
     })
     .delete((req, res) => {
-        fs.access(path.join(__dirname, `./files${req.url}.txt`), fs.constants.F_OK, (err) => {
-            if (err) {
-                res.send('File does not exist');
-            } else {
-                fs.unlink(path.join(__dirname, `./files${req.url}.txt`), (err) => {
-                    if (err) throw err;
-                    res.send(`file has been deleted`);
-                });
-            }
-        });
+        if (checkExistence(req.url)) {
+            fs.unlink(path.join(__dirname, `./files${req.url}.txt`), (err) => {
+                if (err) throw err;
+                res.send(`file has been deleted`);
+            });
+
+        } else {
+            res.send('File does not exist');
+        }
     });
+
+function checkExistence(fileName) {
+    let result;
+
+    try {
+        fs.accessSync(path.join(__dirname, `./files${fileName}.txt`), fs.constants.R_OK | fs.constants.W_OK);
+        result = true;
+    } catch (err) {
+        result = false;
+    }
+
+    return result;
+}
 
 app.listen('3000', () => console.log('Server is running on port 3000'));
